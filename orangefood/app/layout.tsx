ffee2b2fbed490959589d82a42b120efce2f7b9b
Kubrakara <kubra26kara@gmail.com>
2025-02-12 18:10:37 +0300

    Add AdSense verification script to RootLayout component

--- orangefood/app/layout.tsx
+++ orangefood/app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import Head from "next/head";
 
 export default function RootLayout({
   children,
@@ -7,8 +8,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        {/* 📢 AdSense Doğrulama Kodu */}
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8503536557509227"
+          crossOrigin="anonymous"
+        ></script>
+      </Head>
       <body>{children}</body>
     </html>
   );
 }
- 
