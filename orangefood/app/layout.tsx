001fd48019fb9d51115fccbd477f64a0a814bba1
Kubrakara <kubra26kara@gmail.com>
2025-02-12 19:12:07 +0300

    Add Vercel Analytics to RootLayout for enhanced tracking

--- orangefood/app/layout.tsx
+++ orangefood/app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function RootLayout({
   children,
@@ -69,6 +70,7 @@ export default function RootLayout({
         />
       </head>
       <body>{children}</body>
+      <Analytics />
     </html>
   );
 }
